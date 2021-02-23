c2a990ae4298d901d6be0c81de1b2abedfd73ca5
AbineshKThatchinamoorthy <77613419+AbineshKThatchinamoorthy@users.noreply.github.com>
2021-02-23 17:00:56 +0530

    Update ColorCodeMngmnt.c

--- ColorCodeMngmnt.c
+++ ColorCodeMngmnt.c
@@ -11,17 +11,6 @@
 
 #include "ColorCodeMngmnt.h"
 
-const char* MajorColorNames[] = {
-    "White", "Red", "Black", "Yellow", "Violet"
-};
-
-const char* MinorColorNames[] = {
-    "Blue", "Orange", "Green", "Brown", "Slate"
-};
-
-int numberOfMinorColors =
-    sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
-
 void ColorPairToString(const ColorPair* colorPair, char* buffer) {
     sprintf(buffer, "%s %s",
         MajorColorNames[colorPair->majorColor],
@@ -32,14 +21,14 @@ ColorPair GetColorFromPairNumber(int pairNumber) {
     ColorPair colorPair;
     int zeroBasedPairNumber = pairNumber - 1;
     colorPair.majorColor = 
-        (majorColor_t)(zeroBasedPairNumber / numberOfMinorColors);
+        (majorColor_t)(zeroBasedPairNumber / NUM_OF_MIN_COLORS);
     colorPair.minorColor =
-        (minorColor_t)(zeroBasedPairNumber % numberOfMinorColors);
+        (minorColor_t)(zeroBasedPairNumber % NUM_OF_MIN_COLORS);
     return colorPair;
 }
 
 int GetPairNumberFromColor(const ColorPair* colorPair) {
-    return colorPair->majorColor * numberOfMinorColors +
+    return colorPair->majorColor * NUM_OF_MIN_COLORS +
             colorPair->minorColor + 1;
 }
 
