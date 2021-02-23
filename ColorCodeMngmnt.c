95f98e5105cb9b5a8ba386926dfddf010dcb1eb3
AbineshKThatchinamoorthy <77613419+AbineshKThatchinamoorthy@users.noreply.github.com>
2021-02-23 16:53:04 +0530

    Update ColorCodeMngmnt.c

--- ColorCodeMngmnt.c
+++ ColorCodeMngmnt.c
@@ -68,12 +68,10 @@ void testPairToNumber(
     assert(pairNumber == expectedPairNumber);
 }
 
-int main() {
+void main() {
     testNumberToPair(4, WHITE, BROWN);
     testNumberToPair(5, WHITE, SLATE);
 
     testPairToNumber(BLACK, ORANGE, 12);
     testPairToNumber(VIOLET, SLATE, 25);
-
-    return 0;
 }
