dbbb6290f8eb8e130a79c00c5aee5e214ddb527f
AbineshKThatchinamoorthy <77613419+AbineshKThatchinamoorthy@users.noreply.github.com>
2021-02-23 17:37:04 +0530

    Update ColorCodeMngmnt.c

--- ColorCodeMngmnt.c
+++ ColorCodeMngmnt.c
@@ -13,23 +13,23 @@
 /* Project Inlusions */
 #include "ColorCodeMngmnt.h"
 
-void ColorPairToString(const ColorPair* colorPair, char* buffer) {
-    sprintf(buffer, "%s %s",
-        majorColorNames_a[colorPair->majorColor],
-        minorColorNames_a[colorPair->minorColor]);
+void formatToString_v(const colorPair_t* colorPair_p, char* strOutput_p) {
+    sprintf(strOutput_p, "%s %s",
+        majorColorNames_a[colorPair_p->majorColor],
+        minorColorNames_a[colorPair_p->minorColor]);
 }
 
-ColorPair GetColorFromPairNumber(int pairNumber) {
-    ColorPair colorPair;
+colorPair_t GetColorFromPairNumber(int pairNumber) {
+    colorPair_t colorPair_s;
     int zeroBasedPairNumber = pairNumber - 1;
-    colorPair.majorColor = 
+    colorPair_s.majorColor = 
         (majorColor_t)(zeroBasedPairNumber / NUM_OF_MIN_COLORS);
     colorPair.minorColor =
         (minorColor_t)(zeroBasedPairNumber % NUM_OF_MIN_COLORS);
-    return colorPair;
+    return colorPair_s;
 }
 
-int GetPairNumberFromColor(const ColorPair* colorPair) {
+int GetPairNumberFromColor(const colorPair_t* colorPair) {
     return colorPair->majorColor * NUM_OF_MIN_COLORS +
             colorPair->minorColor + 1;
 }
@@ -38,10 +38,10 @@ void testNumberToPair(int pairNumber,
     majorColor_t expectedMajor,
     minorColor_t expectedMinor)
 {
-    ColorPair colorPair = GetColorFromPairNumber(pairNumber);
-    char colorPairNames[MAX_COLORPAIR_NAME_CHARS];
-    ColorPairToString(&colorPair, colorPairNames);
-    printf("Got pair %s\n", colorPairNames);
+    colorPair_t colorPair_s = GetColorFromPairNumber(pairNumber);
+    char colorPairNameStr_a[MAX_COLORPAIR_NAME_CHARS];
+    formatToString_v(&colorPair_s, colorPairNameStr_a);
+    printf("Got pair %s\n", colorPairNameStr_a);
     assert(colorPair.majorColor == expectedMajor);
     assert(colorPair.minorColor == expectedMinor);
 }
@@ -51,7 +51,7 @@ void testPairToNumber(
     minorColor_t minor,
     int expectedPairNumber)
 {
-    ColorPair colorPair;
+    colorPair_t colorPair;
     colorPair.majorColor = major;
     colorPair.minorColor = minor;
     int pairNumber = GetPairNumberFromColor(&colorPair);
