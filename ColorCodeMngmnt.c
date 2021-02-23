e515c93cd65ff844f86fc9eeba1acdd6d9a9e86b
AbineshKThatchinamoorthy <77613419+AbineshKThatchinamoorthy@users.noreply.github.com>
2021-02-23 16:37:00 +0530

    Update and rename main.c to ColorCodeMngmnt.c

--- ColorCodeMngmnt.c
+++ ColorCodeMngmnt.c
@@ -1,25 +1,28 @@
+/******************************************************************************
+/*!
+***     \file        ColorCodeMngmnt.c
+***     \author      Abinesh KanjiKovil Thatchinamoorthy
+***     \brief       Color Code Management for cables
+***    File_description
+***                  
+***
+/*****************************************************************************/
+
 #include <stdio.h>
 #include <assert.h>
 
-enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
-enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
+#include <ColorCodeMngmnt.h>
 
 const char* MajorColorNames[] = {
     "White", "Red", "Black", "Yellow", "Violet"
 };
-int numberOfMajorColors =
-    sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
+
 const char* MinorColorNames[] = {
     "Blue", "Orange", "Green", "Brown", "Slate"
 };
 const int MAX_COLORPAIR_NAME_CHARS = 16;
 int numberOfMinorColors =
     sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
-
-typedef struct {
-    enum MajorColor majorColor;
-    enum MinorColor minorColor;
-} ColorPair;
 
 void ColorPairToString(const ColorPair* colorPair, char* buffer) {
     sprintf(buffer, "%s %s",
@@ -43,8 +46,8 @@ int GetPairNumberFromColor(const ColorPair* colorPair) {
 }
 
 void testNumberToPair(int pairNumber,
-    enum MajorColor expectedMajor,
-    enum MinorColor expectedMinor)
+    majorColor_t expectedMajor,
+    minorColor_t expectedMinor)
 {
     ColorPair colorPair = GetColorFromPairNumber(pairNumber);
     char colorPairNames[MAX_COLORPAIR_NAME_CHARS];
@@ -55,8 +58,8 @@ void testNumberToPair(int pairNumber,
 }
 
 void testPairToNumber(
-    enum MajorColor major,
-    enum MinorColor minor,
+    majorColor_t major,
+    minorColor_t minor,
     int expectedPairNumber)
 {
     ColorPair colorPair;
