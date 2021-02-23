55904830860548696573537cdbf1dd015fe65daf
AbineshKThatchinamoorthy <77613419+AbineshKThatchinamoorthy@users.noreply.github.com>
2021-02-24 05:23:35 +0530

    Update ColorCodeMngmnt.c

--- ColorCodeMngmnt.c
+++ ColorCodeMngmnt.c
@@ -1,7 +1,10 @@
 /******************************************************************************
+*******     (C) COPYRIGHT Robert Bosch GmbH  - All Rights Reserved     ********
+******************************************************************************/
+/*!
 ***     \file        ColorCodeMngmnt.c
 ***     \author      Abinesh KanjiKovil Thatchinamoorthy
-***     \brief       Color Code Management for cables
+***     \brief       Source Code of Color Code Management for cables
 ***    File_description
 ***                  
 *****************************************************************************/
@@ -13,56 +16,56 @@
 /* Project Inlusions */
 #include "ColorCodeMngmnt.h"
 
-void formatToString_v(const colorPair_t* colorPair_p, char* strOutput_p) {
+void formatToString_v(const colorPair_t* colorPair_p, char* strOutput_p) 
+{
     sprintf(strOutput_p, "%s %s",
-        majorColorNames_a[colorPair_p->majorColor],
-        minorColorNames_a[colorPair_p->minorColor]);
+        majorColorNames_a[colorPair_p->majorColor_e],
+        minorColorNames_a[colorPair_p->minorColor_e]);
 }
 
-colorPair_t GetColorFromPairNumber(int pairNumber) {
+colorPair_t getColorFromPairNumber_s(int pairNumber_i) 
+{
     colorPair_t colorPair_s;
-    int zeroBasedPairNumber = pairNumber - 1;
-    colorPair_s.majorColor = 
-        (majorColor_t)(zeroBasedPairNumber / NUM_OF_MIN_COLORS);
-    colorPair_s.minorColor =
-        (minorColor_t)(zeroBasedPairNumber % NUM_OF_MIN_COLORS);
+    int zeroBasedPairNumber_i = pairNumber_i - GENERIC_VALUE_ONE;
+    colorPair_s.majorColor_e = 
+        (majorColor_t)(zeroBasedPairNumber_i / NUM_OF_MIN_COLORS);
+    colorPair_s.minorColor_e =
+        (minorColor_t)(zeroBasedPairNumber_i % NUM_OF_MIN_COLORS);
     return colorPair_s;
 }
 
-int GetPairNumberFromColor(const colorPair_t* colorPair) {
-    return colorPair->majorColor * NUM_OF_MIN_COLORS +
-            colorPair->minorColor + 1;
+int getPairNumberFromColor_i(majorColor_t majorColor_e, minorColor_t minorColor_e) 
+{
+    return ((colorPair_p->majorColor_e * NUM_OF_MIN_COLORS) +
+            colorPair_p->minorColor_e + GENERIC_VALUE_ONE);
 }
 
-void testNumberToPair(int pairNumber,
-    majorColor_t expectedMajor,
-    minorColor_t expectedMinor)
+void testNumberToPair_v(int pairNumber,
+                        majorColor_t expectedMajor,
+                        minorColor_t expectedMinor)
 {
-    colorPair_t colorPair_s = GetColorFromPairNumber(pairNumber);
+    colorPair_t colorPair_s = getColorFromPairNumber_s(pairNumber);
     char colorPairNameStr_a[MAX_COLORPAIR_NAME_CHARS];
     formatToString_v(&colorPair_s, colorPairNameStr_a);
     printf("Got pair %s\n", colorPairNameStr_a);
-    assert(colorPair_s.majorColor == expectedMajor);
-    assert(colorPair_s.minorColor == expectedMinor);
+    assert(colorPair_s.majorColor_e == expectedMajor);
+    assert(colorPair_s.minorColor_e == expectedMinor);
 }
 
-void testPairToNumber(
-    majorColor_t major,
-    minorColor_t minor,
-    int expectedPairNumber)
+void testPairToNumber_v(majorColor_t majorColor_e,
+                        minorColor_t minorColor_e,
+                        int expectedPairNumber_i)
 {
-    colorPair_t colorPair;
-    colorPair.majorColor = major;
-    colorPair.minorColor = minor;
-    int pairNumber = GetPairNumberFromColor(&colorPair);
+    int pairNumber = getPairNumberFromColor_i(majorColor_e, minorColor_e);
     printf("Got pair number %d\n", pairNumber);
-    assert(pairNumber == expectedPairNumber);
+    assert(pairNumber == expectedPairNumber_i);
 }
 
-void main() {
-    testNumberToPair(4, WHITE, BROWN);
-    testNumberToPair(5, WHITE, SLATE);
+void main() 
+{
+    testNumberToPair_v(4, WHITE, BROWN);
+    testNumberToPair_v(5, WHITE, SLATE);
 
-    testPairToNumber(BLACK, ORANGE, 12);
-    testPairToNumber(VIOLET, SLATE, 25);
+    testPairToNumber_v(BLACK, ORANGE, 12);
+    testPairToNumber_v(VIOLET, SLATE, 25);
 }
