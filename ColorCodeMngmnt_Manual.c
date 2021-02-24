17369b58c3e2d0e1662873a15ffb177876b99fe4
AbineshKThatchinamoorthy <77613419+AbineshKThatchinamoorthy@users.noreply.github.com>
2021-02-24 07:35:12 +0530

    Create ColorCodeMngmnt_Manual.c

---
+++ ColorCodeMngmnt_Manual.c
@@ -0,0 +1,34 @@
+/******************************************************************************
+*******     (C) COPYRIGHT Robert Bosch GmbH  - All Rights Reserved     ********
+******************************************************************************/
+/*!
+***     \file        ColorCodeMngmnt.c
+***     \author      Abinesh KanjiKovil Thatchinamoorthy
+***     \brief       Source Code for printing the manual for Color Code
+***                  
+*****************************************************************************/
+
+/* Project Inlusions */
+#include "ColorCodeMngmnt.h"
+
+
+/*=============================================================================
+=======                       FUNCTION DEFINITIONS                      =======
+=============================================================================*/
+
+/*---------------------------------------------------------------------------*/
+/*     FUNCTION:    printColorCodeManual_v
+ */
+/*!    \brief       Print the color code and pair number to serve as a manual
+ *
+ *     \param       void
+ *     \returns     void
+*//*------------------------------------------------------------------------*/
+void printColorCodeManual_v(void)
+{
+  for(int cnt_i = 1; cnt_i <= 25; cnt_i++)
+  {
+    colorPair_t colorPair_s = getColorFromPairNumber_s(cnt_i);
+    printf("%d  %s %s\n", cnt_i, colorPair_s.majorColor_e, colorPair_s.minorColor_e);
+  }
+}
